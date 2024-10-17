f6578aae5fed435e55f6f2c72a3c0d32012aef9d
Paul GAUDEAUX <paul.gaudeaux@proxym.fr>
2024-10-17 16:22:28 +0200

    chore: add database and connect api to it

--- passion-meet-api/src/app.module.ts
+++ passion-meet-api/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+import { TypeOrmConfigService } from './config/database.config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
