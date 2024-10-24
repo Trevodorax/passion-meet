15a57272df366b64d4f19cf3d96de5396728c658
Paul GAUDEAUX <paul.gaudeaux@proxym.fr>
2024-10-24 02:15:11 +0200

    feat: connect create user interface with api

--- passion-meet-android-app/app/src/main/java/com/example/passionmeet/MainActivity.kt
+++ passion-meet-android-app/app/src/main/java/com/example/passionmeet/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var createAccountButton: Button
+    private lateinit var createAccountButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
