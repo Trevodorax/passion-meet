6ec56e4d181adc3b7a79361ff03252923f33e13e
Paul GAUDEAUX <paul.gaudeaux@proxym.fr>
2024-10-26 15:30:18 +0200

    feat: store only hash + login route

--- passion-meet-api/src/user/user.controller.ts
+++ passion-meet-api/src/user/user.controller.ts
@@ -1,11 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { CreateUserDto } from './dto/createUser.dto';
+import { CreateUserDto } from './dto/CreateUser.dto';
+import { LoginDto } from './dto/Login.dto';
 
 interface CreateUserResponse {
   id: string;
   email: string;
   username: string;
+}
+
+interface LoginResponse {
+  token: string
 }
 
 @Controller('user')
@@ -22,5 +27,12 @@ export class UserController {
       email: user.email,
       username: user.username
     }
+  }
+
+  @Post('login')
+  async loginUser(@Body() body: LoginDto): Promise<LoginResponse> {
+    const loginResponse = await this.userService.login(body)
+
+    return loginResponse
   }
 }
