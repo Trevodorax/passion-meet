2d586858d5488a03af7da2194d79ae9d3d4b62d0
Paul GAUDEAUX <paul.gaudeaux@proxym.fr>
2024-10-26 15:55:27 +0200

    feat: add authorization

--- passion-meet-api/src/user/user.module.ts
+++ passion-meet-api/src/user/user.module.ts
@@ -3,12 +3,20 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth.guard';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class UserModule {}
