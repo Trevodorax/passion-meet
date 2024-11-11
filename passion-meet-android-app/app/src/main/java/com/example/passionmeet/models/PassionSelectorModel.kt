1e24ae61a5aadf0556b70309742be837252fc4d8
Clarence HIRSCH <chirsch1@myges.fr>
2024-11-11 18:44:15 +0100

    feat: add SelectPassionActivity and selected passion

--- passion-meet-android-app/app/src/main/java/com/example/passionmeet/models/PassionSelectorModel.kt
+++ passion-meet-android-app/app/src/main/java/com/example/passionmeet/models/PassionSelectorModel.kt
@@ -2,6 +2,7 @@ package com.example.passionmeet.models
 
 data class PassionSelectorModel(
     val name: String,
-    val image: String
+    val image: String,
+    var isSelected: Boolean = false
 ) {
 }
