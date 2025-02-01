fb34b041b8b47945ee4117a528e04765e385c43e
Milo <lhilderal@myges.fr>
2025-02-01 16:14:31 +0100

    removing extra semicolons

--- passion-meet-android-app/app/src/main/java/com/example/passionmeet/views/GroupsVH.kt
+++ passion-meet-android-app/app/src/main/java/com/example/passionmeet/views/GroupsVH.kt
@@ -9,9 +9,9 @@ import com.example.passionmeet.R
 import com.example.passionmeet.models.GroupModel
 
 class GroupsVH(view: View): RecyclerView.ViewHolder(view) {
-    val groupNameTv: TextView = view.findViewById(R.id.group_card_title);
-    val groupMembers: TextView = view.findViewById(R.id.group_card_memberCount);
-    val groupDescription:TextView = view.findViewById(R.id.group_card_description);
+    val groupNameTv: TextView = view.findViewById(R.id.group_card_title)
+    val groupMembers: TextView = view.findViewById(R.id.group_card_memberCount)
+    val groupDescription:TextView = view.findViewById(R.id.group_card_description)
     val groupImage = view.findViewById<ImageView>(R.id.group_card_background)
 
     fun bind(group: GroupModel) {
