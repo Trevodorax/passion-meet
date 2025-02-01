64618ffa792e0348fc29c21ad25a9528f96bd01e
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-02-01 20:06:52 +0100

    adding world map view with Mapbox

--- passion-meet-android-app/app/build.gradle.kts
+++ passion-meet-android-app/app/build.gradle.kts
@@ -73,6 +73,9 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
 
+    // MAPBOX
+    implementation("com.mapbox.maps:android:11.9.1")
+
     // Room components
     val roomVersion = "2.6.1"
     implementation("androidx.room:room-runtime:$roomVersion")
