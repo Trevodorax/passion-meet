3e388cd7ecd0ebee9762a0d3fdd5c29a5e81ac91
Loriane Hilderal <loriane.hilderal@canal-plus.com>
2025-02-01 20:06:52 +0100

    adding world map view with Mapbox

---
+++ passion-meet-android-app/app/src/main/java/com/example/passionmeet/FullMapActivity.kt
@@ -0,0 +1,27 @@
+package com.example.passionmeet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+
+class FullMapActivity: AppCompatActivity() {
+
+    private lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Create a map programmatically and set the initial camera
+        mapView = MapView(this)
+        mapView.mapboxMap.setCamera(
+            CameraOptions.Builder()
+                .center(Point.fromLngLat(2.2, 46.0))
+                .pitch(0.0)
+                .zoom(5.0)
+                .bearing(0.0)
+                .build()
+        )
+        setContentView(mapView)
+    }
+}
