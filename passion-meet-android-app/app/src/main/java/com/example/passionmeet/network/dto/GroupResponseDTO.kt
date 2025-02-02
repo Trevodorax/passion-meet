09a11e489868d04d3070ea4136b8563a07e5ea16
Clarence HIRSCH <chirsch1@myges.fr>
2025-02-02 21:51:32 +0100

    feat: MVVM for getting groups data

---
+++ passion-meet-android-app/app/src/main/java/com/example/passionmeet/network/dto/GroupResponseDTO.kt
@@ -0,0 +1,10 @@
+package com.example.passionmeet.network.dto
+
+data class GroupResponseDTO (
+
+    val id: String,
+    val name: String,
+    val description: String,
+    val imageUrl: String
+
+)
