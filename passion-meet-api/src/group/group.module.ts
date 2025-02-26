876c632e5ef06e124134e1377f6f9ba26b00c1d0
Nino <ninoplane@gmail.com>
2025-02-26 23:03:12 +0100

    added activity module in group module

--- passion-meet-api/src/group/group.module.ts
+++ passion-meet-api/src/group/group.module.ts
@@ -1,13 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Group } from './group.entity';
 import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
 import { PassionModule } from '../passion/passion.module';
+import { ActivityModule } from 'src/activity/activity.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Group]),
+    forwardRef(() => ActivityModule),    
     PassionModule
   ],
   controllers: [GroupController],
